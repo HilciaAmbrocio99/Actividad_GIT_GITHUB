5dfd166161f01f741de631cda49c11645c21aaa6
HilciaAmbrocio99 <hilciaambrocio201805540@gmail.com>
2023-09-16 01:47:04 -0600

    hola

---
+++ hilci.py
@@ -0,0 +1,9 @@
+a = float (input ('Ingresa el valor de a: '))
+b = float (input ('Ingresa el valor de b: '))
+c = float (input ('Ingresa el valor de c: '))
+d = float (input ('Ingresa el valor de d: '))
+e = float (input ('Ingresa el valor de e: '))
+suma=a+b+c+d+e
+promedio=suma/5
+print ('Valor de promedio: ' + repr (promedio))
+print ('Valor de suma: ' + repr (suma))
